1e16f0e1c5545db15cee1b2f854d514b8e690c13
M Banks <montensia1@outlook.com>
2019-06-09 16:16:20 +0000

    Automatically backed up by Learn

--- lib/models/music_importer.rb
+++ lib/models/music_importer.rb
@@ -2,21 +2,24 @@ class MusicImporter
 
   attr_accessor :path
 
-  def initialize (path)
+  def initialize (path = './db/mp3s')
     @path = path
   end
 
   def files
     files = []
-    Dir.new(self.path).each do |file|
-      files << file if file.length > 4
+    Dir.foreach(path) do |file|
+      if file.end_with?('mp3')
+        files << file
+      end
     end
-    files
+   files
   end
 
-  # def import
-  #   self.files.each do |filename|
-  #     Song.new_by_filename(filename)
-  #   end
-  # end
+  def import
+    #binding.pry
+    self.files.each do |filename|
+      Song.create_from_filename(filename)
+    end
+  end
 end
--- lib/models/song.rb
+++ lib/models/song.rb
@@ -42,10 +42,22 @@ class Song
     end
   end
   
-  def self.new_from_filename(name)
-    self.files.each do |filename|
-      Song.new_by_filename(filename)
-    end
+  def self.new_from_filename(filename)
+   # binding.pry
+    song_name = filename.split(" - ")[1]
+    song_artist = filename.split(" - ")[0]
+    song_genre = filename.split(" - ")[2].gsub(".mp3", "") 
+    
+    new_genre = Genre.find_or_create_by_name(song_genre)
+    new_artist = Artist.find_or_create_by_name(song_artist)
+    
+    new_song = Song.new(song_name, new_artist, new_genre)
+   # @artist.add_song(new_song)
+  end
+  
+  def self.create_from_filename(filename)
+    new_from_filename(filename).save
+   
   end
 
 end
